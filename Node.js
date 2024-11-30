438442aab191973a48f1e3b663ef9c3664162d56
Mehedi Hasan <158504182+mehedi-x@users.noreply.github.com>
2024-11-30 23:52:44 +0600

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,18 @@
+{
+  "name": "responsive-store",
+  "version": "1.0.0",
+  "scripts": {
+    "start": "live-server",
+    "build": "webpack",
+    "test": "jest",
+    "deploy": "gh-pages -d build"
+  },
+  "dependencies": {
+    "gh-pages": "^3.2.3",
+    "live-server": "^1.2.1"
+  },
+  "devDependencies": {
+    "webpack": "^5.52.0",
+    "jest": "^26.6.3"
+  }
+}
